b4a1744a38ec7e2e4f17881a13313114fda1f64a
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-14 09:53:45 +0800

    added rename file

---
+++ rename_files.py
@@ -0,0 +1,11 @@
+import os
+
+def rename_files():
+    #1 get files from a folder
+    file_list =  os.listdir(r'C:\Users\Dominic Jay\Documents\Repository\pythonpsm\prank')
+    print file_list
+    #2 for each file, rename filename
+
+
+
+rename_files()
