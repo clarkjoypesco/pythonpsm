8c3e94c7667ff7d11f5e34130e321416a0d9e5a8
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-14 10:07:00 +0800

    file renamed

--- rename_files.py
+++ rename_files.py
@@ -4,8 +4,15 @@ def rename_files():
     #1 get files from a folder
     file_list =  os.listdir(r'C:\Users\Dominic Jay\Documents\Repository\pythonpsm\prank')
     print file_list
+    saved_path = os.getcwd()
+    print 'Current Working Directory: ' + saved_path
+    os.chdir(r'C:\Users\Dominic Jay\Documents\Repository\pythonpsm\prank')
+    for file_name in file_list:
+        print 'Old Name- ' + file_name
+        print 'New Name- ' + file_name.translate(None,'0123456789')
+        os.rename(file_name, file_name.translate(None,'0123456789'))
     #2 for each file, rename filename
-
+    os.chdir = saved_path
 
 
 rename_files()
